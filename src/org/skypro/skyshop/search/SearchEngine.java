8cf77f15b2f012eeb1124bd7c94410ad51cc13ea
Bodnevich <bodnevichkristina@mail.ru>
2025-10-29 14:35:11 +0300

    HM19

--- src/org/skypro/skyshop/search/SearchEngine.java
+++ src/org/skypro/skyshop/search/SearchEngine.java
@@ -1,21 +1,20 @@
 package org.skypro.skyshop.search;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class SearchEngine {
     private List<Searchable> items = new ArrayList<>();
 
     public void add(Searchable item) {
-            items.add(item);
+        items.add(item);
         }
 
-    public List<Searchable> findAllMatching(String searchQuery) {
-        List<Searchable> results = new ArrayList<>();
+    public Map<String, Searchable> findAllMatching(String searchQuery) {
+        Map<String, Searchable> results = new TreeMap<>();
 
         for (Searchable item : items) {
             if (item.getSearchTerm().contains(searchQuery)) {
-                results.add(item);
+                results.put(item.getObjectName(), item);
             }
         }
 
