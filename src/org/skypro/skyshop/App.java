13375f7ba876e1b16d8627c7d8516668f53ecf4e
Bodnevich <bodnevichkristina@mail.ru>
2025-11-03 18:33:13 +0300

    HM20

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -6,7 +6,7 @@ import org.skypro.skyshop.search.SearchEngine;
 import org.skypro.skyshop.article.Article;
 import org.skypro.skyshop.search.Searchable;
 
-import java.util.Map;
+import java.util.Set;
 
 public class App {
     public static void main(String[] args) {
@@ -15,6 +15,13 @@ public class App {
         product.totalCost();
 
         SearchEngine engine = new SearchEngine();
+
+        product.addProduct(new FixPriceProduct("Спагетти"));
+        product.addProduct(new DiscountedProduct("Сливки", 150, 13));
+        product.addProduct(new SimpleProduct("Курица", 385));
+        product.addProduct(new FixPriceProduct("Грибы"));
+        product.addProduct(new DiscountedProduct("Пармезан", 420, 38));
+        product.addProduct(new SimpleProduct("Черри", 109));
 
         engine.add(new FixPriceProduct("Спагетти"));
         engine.add(new DiscountedProduct("Сливки", 150, 13));
@@ -27,10 +34,12 @@ public class App {
         engine.add(new Article("Итальянская кухня", "Рассказ о лучших итальянских блюдах."));
         engine.add(new Article("Сыр пармезан", "Самый известный сыр Италии."));
 
-        Map<String, Searchable> results = engine.findAllMatching("Пармезан");
+        Set<Searchable> results = engine.findAllMatching("Пармезан");
         System.out.println("\nРезультаты поиска по запросу: ");
-        for (Map.Entry<String, Searchable> result : results.entrySet()) {
-            System.out.println(result.getValue().getStringRepresentation());
+        for (Searchable result : results) {
+            System.out.println(result.getStringRepresentation());
         }
+
+        System.out.println("\nОбщая стоимость товаров в корзине: " + product.totalCost());
     }
 }
